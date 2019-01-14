68e469ff71d0963ecce06a00f189ff0d4b00278d
Randika <randikapw@gmail.com>
2019-01-15 00:38:53 +0530

    High level listening for DragEvent

---
+++ Lib/CodeLab2/DagDetector.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(DragListner());
+}
+
+class DragListner extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DragableTester',
+      home: SwipableView(),
+    );
+  }
+}
+
+class SwipableView extends StatefulWidget {
+
+  @override
+  SwipableViewState createState() => SwipableViewState();
+}
+
+class SwipableViewState extends State<SwipableView> {
+
+  String _message = 'New message';
+
+  void _updateMessage(String newMessage){
+    setState(() {
+      _message = newMessage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragUpdate: (DragUpdateDetails details){
+        _updateMessage('Dragging');
+      },
+      onHorizontalDragEnd: (DragEndDetails details){
+        _updateMessage('Waiting for a drag');
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue[100]
+        ),
+        child: Center(
+          child: Text(_message),
+        ),
+      ),
+    );
+  }
+}
